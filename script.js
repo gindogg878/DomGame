8ffa36bd261c561f08707585d378eb1c74493dcd
Gino Torres Jr <gindogg878@icloud.com>
2023-03-24 01:05:32 -0500

    added some styles to the game

--- script.js
+++ script.js
@@ -3,25 +3,25 @@ const gallery = [
   {
     text: "animals",
     imgArr: [
-      { text: "elephant", link: "https://i.imgur.com/VDld0dD.jpeg" },
-      { text: "zebra", link: "https://i.imgur.com/Rkv1bdQ.jpeg" },
-      { text: "tiger", link: "https://i.imgur.com/GrpqkHM.jpeg" },
+      { text: "elephant", link: "imgs/elephant.jpg" },
+      { text: "zebra", link: "imgs/zebra.jpeg" },
+      { text: "tiger", link: "imgs/tiger.jpg" },
     ],
   },
   {
     text: "people",
     imgArr: [
-      { text: "harry", link: "https://i.imgur.com/GrpqkHM.jpeg" },
-      { text: "sally", link: "https://i.imgur.com/GrpqkHM.jpeg" },
-      { text: "mary", link: "https://i.imgur.com/GrpqkHM.jpeg" },
+      { text: "prince", link: "imgs/prince.jpg" },
+      { text: "adelle", link: "imgs/Adele.jpg" },
+      { text: "drake", link: "imgs/Drake.jpg" },
     ],
   },
   {
     text: "places",
     imgArr: [
-      { text: "paris", link: "https://i.imgur.com/GrpqkHM.jpeg" },
-      { text: "rome", link: "https://i.imgur.com/GrpqkHM.jpeg" },
-      { text: "cairo", link: "https://i.imgur.com/GrpqkHM.jpeg" },
+      { text: "paris", link: "imgs/paris.jpg" },
+      { text: "rome", link: "imgs/rome.jpg" },
+      { text: "new york", link: "imgs/newyork.jpg" },
     ],
   },
 ];
@@ -42,6 +42,8 @@ const wrongGuessDiv = document.querySelector(".wrong-guess");
 const skipDiv = document.querySelector(".skipBtn");
 const lowerPage = document.getElementById("lowerPage");
 const selectCat = document.getElementById("randomCat");
+const turnStyle = document.querySelector(".player1");
+const turnStyle2 = document.querySelector(".player2");
 let storeGuess1 = document.getElementById("guess1");
 let storeGuess2 = document.getElementById("guess2");
 const submitBtn1 = document.getElementById("btn1");
@@ -59,7 +61,8 @@ eraseH1.addEventListener("click", (e) => {
   if (e.target) {
     //will remove h1//
     eraseH1.remove();
-    console.log(filter);
+    document.body.style.backgroundImage = "none";
+    document.body.style.backgroundColor = "#E3DFFD";
     //div container with the image to be guessed placed as a background img//
     imageContainer.style.display = "grid";
     //loop creating 25 divs to cover image to be guessed//
@@ -80,14 +83,18 @@ eraseH1.addEventListener("click", (e) => {
     if (whoseTurn === 1) {
       ranH1.textContent = "Player 1 will go first";
       randomDiv.style.display = "flex";
+      turnStyle.style.backgroundColor = "#DFFFD8";
+
       function eraseDiv() {
         whoseTurn.value = "";
         randomDiv.style.display = "none";
+        turnStyle.style.backgroundColor = "none";
       }
       setTimeout(eraseDiv, 3250);
     } else if (whoseTurn === 2) {
       ranH1.textContent = "Player 2 will go first";
       randomDiv.style.display = "flex";
+      turnStyle2.style.backgroundColor = "#DFFFD8";
       function eraseDiv() {
         whoseTurn.value = "";
         randomDiv.style.display = "none";
@@ -107,11 +114,10 @@ eraseH1.addEventListener("click", (e) => {
     let choice3 = gallery[choice].imgArr[choice2].link;
     console.log(choice3);
     imageContainer.style.backgroundImage = `url('${choice3}')`;
-    console.log(imageContainer);
 
     // //trying to create a EL to capture the id of clicked element and remove//
     const cell = document.querySelectorAll(".cells");
-    console.log(cell);
+    // console.log(cell);
     filter.addEventListener("click", (e) => {
       if (e.target) {
         // filter.removeChild(e.target);
@@ -137,10 +143,19 @@ eraseH1.addEventListener("click", (e) => {
         corH1.textContent =
           "Player 1 guessed correctly. Congratulations you WON!!!";
         winnerDiv.style.display = "flex";
+        //trying to make all cells transparent when guess is correct//
+        // const allCell = document.querySelectorAll(".cells");
+        // console.log(allCell);
+        // allCell.style.backgroundColor = "transparent";
+        // cell.style.backgroundColor.bind = "transparent";
+        // console.log(cell);
       } else if (!storeGuess1.value || storeGuess1 !== answer) {
         wrongH2.textContent =
           "Player 1 guessed incorrectly. Next players turn.";
         wrongGuessDiv.style.display = "flex";
+        // turnStyle2.style.backgroundColor = "yellow";
+        // turnStyle.style.backgroundColor = "";
+        console.log(turnStyle);
         function eraseDiv() {
           storeGuess1.value = "";
           wrongGuessDiv.style.display = "none";
@@ -160,6 +175,8 @@ eraseH1.addEventListener("click", (e) => {
         wrongH2.textContent =
           "Player 2 guessed incorrectly. Next players turn.";
         wrongGuessDiv.style.display = "flex";
+        turnStyle.style.backgroundColor = "#DFFFD8";
+        turnStyle2.style.backgroundColor = "none";
         function eraseDiv() {
           wrongGuessDiv.style.display = "none";
         }
@@ -171,7 +188,9 @@ eraseH1.addEventListener("click", (e) => {
   skip1.addEventListener("click", (e) => {
     if (e.target) {
       skiH2.textContent = "Player 1 skipped turn. Next players turn.";
-      skipDiv.style.display = "block";
+      skipDiv.style.display = "flex";
+      // turnStyle2.style.backgroundColor = "yellow";
+      // turnStyle.style.backgroundColor = "none";
       function eraseDiv() {
         skipDiv.style.display = "none";
       }
@@ -183,7 +202,9 @@ eraseH1.addEventListener("click", (e) => {
   skip2.addEventListener("click", (e) => {
     if (e.target) {
       skiH2.textContent = "Player 2 skipped turn. Next players turn.";
-      skipDiv.style.display = "block";
+      skipDiv.style.display = "flex";
+      turnStyle.style.backgroundColor = "yellow";
+      turnStyle2.style.backgroundColor = "none";
       function eraseDiv() {
         skipDiv.style.display = "none";
       }
